5e82099e6766bc690d1caec32f5cac8fc2bbeab4
Karthikeya <145262370+weasel-0@users.noreply.github.com>
2023-10-07 20:12:28 +0530

    got it working with bad ui

--- script2.js
+++ script2.js
@@ -17,6 +17,25 @@ function getComputerChoice() {
 const rock = document.querySelector("#rockBtn");
 const paper = document.querySelector("#paperBtn");
 const scissors = document.querySelector("#scissorsBtn");
+const scores = document.querySelector(".scores");
+const pscore = document.querySelector(".pscore");
+const cscore = document.querySelector(".cscore");
+const winner = document.querySelector(".winner");
+const reset = document.querySelector(".tryAgain");
+const card = document.querySelector(".card");
+let newMessage = document.querySelector(".message");
+let liveFeed = document.getElementById("liveFeed");
+reset.style.display = "none";
+
+const resetGame = reset.addEventListener("click", function () {
+  playerScore = 0;
+  computerScore = 0;
+  pscore.textContent = playerScore;
+  cscore.textContent = computerScore;
+  winner.textContent = "";
+  reset.style.display = "none";
+  newMessage.textContent = "";
+});
 
 let playerSelection;
 let choseRock;
@@ -28,45 +47,92 @@ let computerScore = 0;
 
 choseRock = rock.addEventListener("click", function () {
   playRound("rock", getComputerChoice());
+  // this.parentElement.style.backgroundColor = "yellow";
+  checkScore(playerScore, computerScore);
+  // gameStatus("rock", computerSelection);
 });
 chosePaper = paper.addEventListener("click", function () {
   playRound("paper", getComputerChoice());
+  checkScore(playerScore, computerScore);
+  // gameStatus("paper", computerSelection);
 });
 choseScissors = scissors.addEventListener("click", function () {
   playRound("scissors", getComputerChoice());
+  checkScore(playerScore, computerScore);
+  // gameStatus("scissors", computerSelection);
 });
-console.log(playerScore, computerScore);
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === "rock" && computerSelection === "rock") {
-    result = "tie";
+    newMessage.textContent = `It's a tie! you both chose rock`;
+    return "tie";
   } else if (playerSelection === "rock" && computerSelection === "paper") {
     computerScore++;
+    newMessage.textContent = `You have lost human! `;
+
+    cscore.textContent = computerScore;
     return "computer won";
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
     playerScore++;
+    newMessage.textContent = `You won! might be a fluke.. `;
+
+    pscore.textContent = playerScore;
+
     return "player won";
   } else if (playerSelection === "paper" && computerSelection === "rock") {
     playerScore++;
+    pscore.textContent = playerScore;
+    newMessage.textContent = `You won! just a coincidence `;
+
     return "player won";
   } else if (playerSelection === "paper" && computerSelection === "paper") {
+    newMessage.textContent = `It's a tie`;
+
     return "tie";
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
     computerScore++;
+    cscore.textContent = computerScore;
+    newMessage.textContent = `you have lost ..even a toddler could do better than that `;
+
     return "computer won";
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
     computerScore++;
+    cscore.textContent = computerScore;
+    newMessage.textContent = `you have lost! skill issue.. `;
+
     return "computer won";
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
     playerScore++;
+    pscore.textContent = playerScore;
+    newMessage.textContent = `You won! won't happen again `;
+
     return "player won";
   } else if (
     playerSelection === "scissors" &&
     computerSelection === "scissors"
   ) {
+    newMessage.textContent = `It's a tie`;
+
     return "tie";
   } else {
     return "invalid choice";
   }
   //   return result;
+}
+
+function checkScore(playerScore, computerScore) {
+  if (playerScore === 5) {
+    console.log("player won");
+    newMessage.textContent = "";
+    winner.textContent = `How is this possible? I want a rematch, unless you're scared.`;
+    reset.style.display = "block";
+  } else if (computerScore === 5) {
+    console.log("compuuter won");
+    newMessage.textContent = "";
+
+    winner.textContent = `Better luck next time, kid.`;
+    reset.style.display = "block";
+  } else {
+    // card.style.backgroundColor = "white";
+  }
 }
