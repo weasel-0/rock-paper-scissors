7897ab6b1eb83c6c0b8d6f97a174599bcfb086d5
Karthikeya <145262370+weasel-0@users.noreply.github.com>
2023-10-07 20:19:11 +0530

    file cleanup

--- script.js
+++ script.js
@@ -1,97 +1,138 @@
-"use strict";
-console.log("Hello");
+/* TODO
+1. make a button for each opttion
+2. add an event listener which upon btn click saves the option as player selection .
+3. generate a random option for the computer's choice
+4. show who won the match
+5. increment the score 
+6. do this until one reaches score - 5
 
-/* problem - make rock-paper-scissors 
-1. get input from user, store it
-2. computer must make a random choice 
-3. check for the conditions to determine who won
-4. display score - 0 --> increment if won 
-5. max score - 5
 */
-
 let choices = ["rock", "paper", "scissors"];
-let randomNum;
-let computerSelection;
-let playerSelection;
-let result;
-
-const playerOption = document.querySelectorAll(".btn");
-function choice() {
-  playerSelection = playerOption.textContent;
-  console.log(playerSelection);
-}
-playerOption.forEach((option) => option.addEventListener("click", choice));
-// computer's choice
+let computerSelection = getComputerChoice();
 function getComputerChoice() {
-  randomNum = Math.floor(Math.random() * choices.length);
+  let randomNum = Math.floor(Math.random() * choices.length);
   return choices[randomNum];
 }
 
-//game function for a single round
-function playAround(playerSelection, computerSelection) {
+const rock = document.querySelector("#rockBtn");
+const paper = document.querySelector("#paperBtn");
+const scissors = document.querySelector("#scissorsBtn");
+const scores = document.querySelector(".scores");
+const pscore = document.querySelector(".pscore");
+const cscore = document.querySelector(".cscore");
+const winner = document.querySelector(".winner");
+const reset = document.querySelector(".tryAgain");
+const card = document.querySelector(".card");
+let newMessage = document.querySelector(".message");
+let liveFeed = document.getElementById("liveFeed");
+reset.style.display = "none";
+
+const resetGame = reset.addEventListener("click", function () {
+  playerScore = 0;
+  computerScore = 0;
+  pscore.textContent = playerScore;
+  cscore.textContent = computerScore;
+  winner.textContent = "";
+  reset.style.display = "none";
+  newMessage.textContent = "";
+});
+
+let playerSelection;
+let choseRock;
+let chosePaper;
+let choseScissors;
+let result;
+let playerScore = 0;
+let computerScore = 0;
+
+choseRock = rock.addEventListener("click", function () {
+  playRound("rock", getComputerChoice());
+  // this.parentElement.style.backgroundColor = "yellow";
+  checkScore(playerScore, computerScore);
+  // gameStatus("rock", computerSelection);
+});
+chosePaper = paper.addEventListener("click", function () {
+  playRound("paper", getComputerChoice());
+  checkScore(playerScore, computerScore);
+  // gameStatus("paper", computerSelection);
+});
+choseScissors = scissors.addEventListener("click", function () {
+  playRound("scissors", getComputerChoice());
+  checkScore(playerScore, computerScore);
+  // gameStatus("scissors", computerSelection);
+});
+
+function playRound(playerSelection, computerSelection) {
   if (playerSelection === "rock" && computerSelection === "rock") {
-    result = "tie";
+    newMessage.textContent = `It's a tie! you both chose rock`;
+    return "tie";
   } else if (playerSelection === "rock" && computerSelection === "paper") {
-    result = "computer won";
+    computerScore++;
+    newMessage.textContent = `You have lost human! `;
+
+    cscore.textContent = computerScore;
+    return "computer won";
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    result = "player won";
+    playerScore++;
+    newMessage.textContent = `You won! might be a fluke.. `;
+
+    pscore.textContent = playerScore;
+
+    return "player won";
   } else if (playerSelection === "paper" && computerSelection === "rock") {
-    result = "player won";
+    playerScore++;
+    pscore.textContent = playerScore;
+    newMessage.textContent = `You won! just a coincidence `;
+
+    return "player won";
   } else if (playerSelection === "paper" && computerSelection === "paper") {
-    result = "tie";
+    newMessage.textContent = `It's a tie`;
+
+    return "tie";
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    result = "computer won";
+    computerScore++;
+    cscore.textContent = computerScore;
+    newMessage.textContent = `you have lost ..even a toddler could do better than that `;
+
+    return "computer won";
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    result = "computer won";
+    computerScore++;
+    cscore.textContent = computerScore;
+    newMessage.textContent = `you have lost! skill issue.. `;
+
+    return "computer won";
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    result = "player won";
+    playerScore++;
+    pscore.textContent = playerScore;
+    newMessage.textContent = `You won! won't happen again `;
+
+    return "player won";
   } else if (
     playerSelection === "scissors" &&
     computerSelection === "scissors"
   ) {
-    result = "tie";
+    newMessage.textContent = `It's a tie`;
+
+    return "tie";
   } else {
-    result = "invalid choice";
+    return "invalid choice";
   }
-  return result;
+  //   return result;
 }
 
-//calling the function for multiple rounds
-let playerScore = 0;
-let computerScore = 0;
-let roundNumber = 1;
+function checkScore(playerScore, computerScore) {
+  if (playerScore === 5) {
+    console.log("player won");
+    newMessage.textContent = "";
+    winner.textContent = `How is this possible? I want a rematch, unless you're scared.`;
+    reset.style.display = "block";
+  } else if (computerScore === 5) {
+    console.log("compuuter won");
+    newMessage.textContent = "";
 
-/*
-for (let i = 0; i < 5; i++) {
-  computerSelection = getComputerChoice();
-  playerSelection = prompt("Enter your choice - rock,paper,scissors ");
-  playerSelection = playerSelection.toLowerCase();
-  playAround(playerSelection, computerSelection);
-  if (result === "computer won") {
-    computerScore++;
-  } else if (result === "player won") {
-    playerScore++;
+    winner.textContent = `Better luck next time, kid.`;
+    reset.style.display = "block";
+  } else {
+    // card.style.backgroundColor = "white";
   }
-  console.log(`---------- Round ${roundNumber} ----------`);
-  console.log(`Player Selected - ${playerSelection}`);
-  console.log(`Computer Selected - ${computerSelection}`);
-  console.log(`Result - ${result}`);
-
-  roundNumber++;
-  //   console.log(playAround(playerSelection, computerSelection));
 }
-
-//printing out winner
-console.log(
-  `Player Scored - ${playerScore}, Computer Scored - ${computerScore}`
-);
-
-let winner;
-if (playerScore > computerScore) {
-  console.log(`Player won 🎉`);
-} else if (playerScore < computerScore) {
-  console.log(`Computer won 🤖 `);
-} else {
-  console.log("Go for another round!!");
-}
-*/
